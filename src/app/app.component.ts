0e88eb39e365eb891c766d84ee156509a4892f68
petr <stechy12@gmail.com>
2019-07-08 12:17:41 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'frontend';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // setInterval(() => {
+      this.http.get('/api/admin/dashboard/users')
+          .toPromise()
+          .then(value => {
+            console.log(value);
+          });
+    // }, 2000);
+
+  }
+}
