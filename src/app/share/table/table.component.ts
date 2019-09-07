a7ee5417c36afd372442c83bbf418357f19016cd
petr <stechy12@gmail.com>
2019-09-07 15:12:49 +0200

    Odstranění zbytečného logování.

--- src/app/share/table/table.component.ts
+++ src/app/share/table/table.component.ts
@@ -1,11 +1,11 @@
-import { AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit, AfterViewChecked {
+export class TableComponent implements OnInit {
 
   @Input() headerColor: 'success' | 'warning' | 'danger' = 'success';
   @Input() title: string;
@@ -20,13 +20,7 @@ export class TableComponent implements OnInit, AfterViewChecked {
   ngOnInit() {
   }
 
-  ngAfterViewChecked(): void {
-    console.log(this.tableRows);
-  }
-
   get tableHasCustomContent(): boolean {
-    // console.log(this.tableContent);
     return this.tableRows === undefined;
-    // return this.tableContent ? this.tableContent.nativeElement.children.length !== 0 : false;
   }
 }
