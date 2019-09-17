e40aed0c2575d4722ae2eb4ba027a3709f1380a2
petr <stechy12@gmail.com>
2019-09-17 14:08:14 +0200

    Dynamické zobrazení nabízených služeb.

--- src/app/general/fragments/services/services.component.ts
+++ src/app/general/fragments/services/services.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LectureType } from '../../general.share';
+import { ServicesService } from './services.service';
 
 @Component({
   selector: 'app-services',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  lectureTypes: LectureType[] = [];
+
+  constructor(private _service: ServicesService) { }
 
   ngOnInit() {
+    this._service.lectureTypes()
+        .then(lectureTypes => {
+          this.lectureTypes = lectureTypes;
+        });
+
   }
 
 }
---
+++ src/app/general/general.share.ts
@@ -0,0 +1,8 @@
+export const BASE_GENERAL_API = '/api/general';
+
+export interface LectureType {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+}
