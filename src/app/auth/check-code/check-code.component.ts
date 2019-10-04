0ed91c5e9bc917a538f1ccf575def7f03975cacd
petr <stechy12@gmail.com>
2019-10-04 10:57:44 +0200

    Dokončení ověření e-mailu.

--- src/app/auth/check-code/check-code.component.ts
+++ src/app/auth/check-code/check-code.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-check-code',
@@ -13,20 +14,29 @@ export class CheckCodeComponent implements OnInit {
     checkCode: new FormControl('', [Validators.required])
   });
 
-  constructor(private _auth: AuthService, private _route: ActivatedRoute, private _router: Router) { }
+  constructor(private _auth: AuthService, private _toastr: ToastrService,
+              private _route: ActivatedRoute, private _router: Router) {
+  }
+
+  private _checkCode(code: string): void {
+    this._auth.checkCode(code)
+        .then(() => {
+          return this._router.navigate(['/auth']);
+        })
+        .then(() => {
+          this._toastr.success('Váš e-mail byl úspěšně ověřen.');
+        });
+  }
 
   ngOnInit() {
     const code = this._route.snapshot.params.code;
     if (code !== undefined) {
-      this._auth.checkCode(code)
-          .then(() => {
-            this._router.navigate(['/auth']);
-          });
+      this._checkCode(code);
     }
   }
 
   handleCheckCode() {
-
+    this._checkCode(this.checkCodeForm.value.checkCode);
   }
 
   get checkCode() {
